5a2f92dee579efc2b4f023f231955d21bd5bf71d
TanatornG <nicolas.sempere@free.fr>
2023-03-12 23:54:30 +0100

    Correction de l'arbre du projet dans le README.md

--- _build/default/expr/basic/toPfx.ml
+++
@@ -1,7 +0,0 @@
-open Ast
-
-let generate = function
-  | Const _ -> failwith "To implement"
-  | Binop(_,_,_) -> failwith "To implement"
-  | Uminus _ -> failwith "To implement"
-  | Var _ -> failwith "Not yet supported"
