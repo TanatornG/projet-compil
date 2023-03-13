8e15d2a3df0ac021f7600dc80a564babe773d54a
TanatornG <nicolas.sempere@free.fr>
2023-03-13 17:33:59 +0100

    Exo 5 - Implémentation d'un compilateur simple de Expr vers Pfx

--- expr/basic/toPfx.ml
+++ expr/basic/toPfx.ml
@@ -1,7 +1,14 @@
 open Ast
+open BasicPfx.Ast
 
-let generate = function
-  | Const _ -> failwith "To implement"
-  | Binop(_,_,_) -> failwith "To implement"
-  | Uminus _ -> failwith "To implement"
-  | Var _ -> failwith "Not yet supported"
+(* Auteur : Nicolas Sempéré *)
+(* Question 5.2 - Fonction permettant de passer de Expr à Pfx *)
+let rec generate = function
+  | Const v -> [Push v]
+  | Binop(Badd,expr1,expr2) ->  (generate expr2)@(generate expr1)@[Add]
+  | Binop(Bsub,expr1,expr2) ->  (generate expr2)@(generate expr1)@[Sub]
+  | Binop(Bmul,expr1,expr2) ->  (generate expr2)@(generate expr1)@[Mul]
+  | Binop(Bdiv,expr1,expr2) ->  (generate expr2)@(generate expr1)@[Div]
+  | Binop(Bmod,expr1,expr2) ->  (generate expr2)@(generate expr1)@[Rem]
+  | Uminus expr ->  (generate expr)@[Push 0]@[Sub]
+  | Var _ ->  [Push 0; Pop] (* A implémenter : Push 0 - Pop revient à ne rien faire*)
